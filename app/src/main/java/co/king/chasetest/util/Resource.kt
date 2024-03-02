437a76dec130cb9917b4e618f196ade331645837
kingsleyjohn <nwankwo.kingsleyjohn@gmail.com>
2024-03-02 09:21:35 +0000

    create data resource util

---
+++ app/src/main/java/co/king/chasetest/util/Resource.kt
@@ -0,0 +1,7 @@
+package co.king.chasetest.util
+
+sealed class Resource<T>(data: T?, error: String?) {
+    class Loading<T>: Resource<T>(null, null)
+    data class Success<T>(val data: T?): Resource<T>(data = data, error = null)
+    data class Failure<T>(val data: T?, val error: String): Resource<T>(data = data,error = error)
+}
